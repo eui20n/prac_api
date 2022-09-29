157ac638612d065dd55fe6533c55c7b0a9692d3c
eui20n <eui20n@naver.com>
2022-09-29 21:04:24 +0900

    first commit

---
+++ src/main/java/com/example/prac_api/PracApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.prac_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prac_api/PracApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prac_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
